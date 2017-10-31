f7ac31bba13405cd54c2f2b535043a46e915b631
Hugh Salimbeni <hughsalimbeni@gmail.com>
2017-10-31 10:44:44 +0100

    initial commit

---
+++ answers.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use this file for your answers. 
+
+This file should been in the root of the repository 
+(do not move it or change the file name) 
+
+"""
+
+import numpy as np
+
+def lml(alpha, beta, Phi, Y):
+    """
+    4 marks
+
+    :param alpha: float
+    :param beta: float
+    :param Phi: array of shape (N, M)
+    :param Y: array of shape (N, 1)
+    :return: the log marginal likelihood, a scalar
+    """
+    pass
+
+def grad_lml(alpha, beta, Phi, Y):
+    """
+    8 marks (4 for each component)
+
+    :param alpha: float
+    :param beta: float
+    :param Phi: array of shape (N, M)
+    :param Y: array of shape (N, 1)
+    :return: array of shape (2,). The components of this array are the gradients
+    (d_lml_d_alpha, d_lml_d_beta), the gradients of lml with respect to alpha and beta respectively.
+    """
+    pass
+
